a6a951f551f2db08e035aec1b9d90bd2ba2d3cad
MIKAEL ROOS <mosstud@h430-0003>
2014-05-08 15:14:35 +0200

    fixing h430

---
+++ script/ubuntu_h430.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+FILES="
+.cshrc
+.aliases
+.bash_aliases
+.xsession
+.tmux.conf
+"
+
+for i in $FILES
+do
+	echo "cp ./$i $HOME/$i"
+	cp ./$i $HOME/$i
+done
+
+sudo rsync -v linux/ /
+sudo chmod 640 /etc/sudoers.d/*
+
