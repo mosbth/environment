9120493acbd75b31787b67d4d97a6ae61c418ae8
MIKAEL ROOS <mosstud@h430-0003>
2014-05-15 11:12:49 +0200

    update h430

--- script/setup_ubuntu_14.04_h430.sh
+++ script/setup_ubuntu_14.04_h430.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
-sudo apt-get install git
 
-sudo dpkg --add-architecture i386
-sudo apt-get update
-sudo apt-get install skype
+#spotify key
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 94558F59
+
+sudo apt-get update 
+
+#sudo apt-get install git
+
+#sudo dpkg --add-architecture i386
+#sudo apt-get update
+#sudo apt-get install skype
 
 sudo apt-get install gconf-edit
 
+sudo apt-get install indicator-appmenu
 sudo apt-get install unity-tweak-tool
 sudo apt-get install gnome-tweak-tool
-sudo apt-get install indicator-appmenu
 
 sudo apt-get install compizconfig-settings-manager
+
+sudo apt-get install spotify-client
 
